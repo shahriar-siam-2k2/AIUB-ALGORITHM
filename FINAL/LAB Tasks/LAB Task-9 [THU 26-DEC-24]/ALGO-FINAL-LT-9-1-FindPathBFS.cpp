6d093a626741daee9aba4c88d56a13e38ab6d713
Shahriar Ali Siam <shahriaraliseam@gmail.com>
2024-12-26 15:31:10 +0600

    ALGO-FINAL-LT-9-1 Send to DESKTOP

---
+++ FINAL/LAB Tasks/LAB Task-9 [THU 26-DEC-24]/ALGO-FINAL-LT-9-1-FindPathBFS.cpp
@@ -0,0 +1,149 @@
+// ALGO-FINAL-LT-9-1-FindPathBFS
+
+#include <iostream>
+using namespace std;
+
+const int maxNode = 100;
+bool adjList[maxNode][maxNode];
+bool visited[maxNode];
+int parent[maxNode];
+
+struct Queue {
+    int data[maxNode];
+    int front, rear;
+
+    Queue() {
+        front = 0;
+        rear = 0;
+    }
+
+    void enqueue(int value) {
+        if(rear >= maxNode) {
+            cout << endl << "\tQueue Overflow!" << endl;
+        }
+        data[rear++] = value;
+    }
+
+    int dequeue() {
+        if (isEmpty()) {
+            cout << endl << "\tQueue underflow!" << endl;
+            return -1;
+        }
+        return data[front++];
+    }
+
+    bool isEmpty() {
+        if(front == rear) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
+
+void setGraph(int nodes) {
+    for(int i=0; i<nodes; i++) {
+        for(int j=0; j<nodes; j++) {
+            adjList[i][j] = false;
+        }
+        visited[i] = false;
+        parent[i] = -1;
+    }
+    cout << endl << "\tGraph set!" << endl;
+}
+
+void addEdge(int u, int v, int nodes) {
+    if(u < 0 || u >= nodes || v < 0 || v >= nodes) {
+        cout << endl << "\tInvalid edge! Nodes should be between 0 and " << nodes - 1 << "." << endl;
+        return;
+    }
+    adjList[u][v] = true;
+    adjList[v][u] = true;
+    cout << endl << "\tEdge Added!" << endl << endl;
+}
+
+bool bfs(int start, int dest, int nodes) {
+    for(int i=0; i<nodes; i++) {
+        visited[i] = false;
+        parent[i] = -1;
+    }
+
+    Queue q;
+
+    q.enqueue(start);
+    visited[start] = true;
+
+    while(!q.isEmpty()) {
+        int curr = q.dequeue();
+
+        if(curr == dest) {
+            return true;
+        }
+
+        for(int i=0; i<nodes; i++) {
+            if(adjList[curr][i] && !visited[i]) {
+                q.enqueue(i);
+                visited[i] = true;
+                parent[i] = curr;
+            }
+        }
+    }
+
+    return false;
+}
+
+void showPath(int start, int dest) {
+    if(start == dest) {
+        cout << start << " ";
+        return;
+    }
+    showPath(start, parent[dest]);
+    cout << dest << " ";
+}
+
+int main() {
+    int start, dest, u, v, nodes, edges;
+
+    cout << endl << "Enter the number of nodes: ";
+    cin >> nodes;
+
+    if(nodes <= 0 || nodes > maxNode) {
+        cout << endl << "\tInvalid number of nodes! Must be between 1 and " << maxNode << "." << endl;
+        return 0;
+    }
+
+    setGraph(nodes);
+
+    cout << endl << "Enter the number of edges: ";
+    cin >> edges;
+
+    cout << endl << "Enter the starting point and ending point of following edges-" << endl;
+    for(int i=0; i<edges; i++) {
+        cout << "Edge-" << i+1 << ": ";
+        cin >> u >> v;
+        addEdge(u, v, nodes);
+    }
+
+    cout << endl << "Enter starting node: ";
+    cin >> start;
+    cout << "Enter destination node: ";
+    cin >> dest;
+
+    if(start < 0 || start >= nodes || dest < 0 || dest >= nodes) {
+        cout << endl << "\tInvalid start or destination node! Must be between 0 and " << nodes - 1 << "." << endl;
+        return 0;
+    }
+
+    if(bfs(start, dest, nodes)) {
+        cout << endl << "\tPath Found!" << endl << endl << "Path: ";
+        showPath(start, dest);
+    }
+    else {
+        cout << endl << "\tNo path found from " << start << " to " << dest << endl;
+    }
+
+    cout << endl;
+
+    return 0;
+}
